6377fb013555f2f9b13a508f6f840a4b617031f7
Josh(ua) Christensen <joshua@codingwithjosh.com>
2022-04-30 22:07:19 -0500

    Added in product list, need to fix styles

---
+++ components/ProductList/Card.vue
@@ -0,0 +1,34 @@
+<template>
+  <nuxt-link :to="product.href">
+    <div class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+      <img :src="product.imageSrc" :alt="product.imageAlt" class="w-full h-full object-center object-cover lg:w-full lg:h-full" />
+    </div>
+
+    <div class="mt-4 flex fustify-between">
+      <h3 class="text-sm text-gray-700">{{ product.name }}</h3>
+      <p class="mt-1 text-sm text-gray-500">{{ product.color }}</p>
+      <p class="text-sm font-medium text-gray-900">{{ product.price }}</p>
+    </div>
+  </nuxt-link>
+</template>
+
+<script lang="ts">
+interface products {
+  id: number
+  name: string
+  href: string
+  imageSrc: string
+  imageAlt: string
+  price: number
+  color: string
+}
+
+export default {
+  props: {
+    product: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
---
+++ components/ProductList/ProductList.vue
@@ -0,0 +1,64 @@
+<template>
+  <div v-for="(info, index) in products" :key="index" class="group relative">
+    <ProductListCard :product="info" />
+  </div>
+</template>
+
+<script lang="ts">
+  interface product {
+    id: number,
+    name: string,
+    href: string,
+    imageSrc: string,
+    imageAlt: string,
+    price: number,
+    color: string
+  }
+
+  const products:product[] = [
+    {
+      id: 1,
+      name: 'Basic Black Tee',
+      href: `/products/1`,
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+      imageAlt: "Front of women's Basic Tee in Black.",
+      price: 35,
+      color: 'Black',
+    },
+    {
+      id: 2,
+      name: 'Basic Aspen White Tee',
+      href: '#',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-02.jpg',
+      imageAlt: "Front of women's Basic Tee in Aspen White.",
+      price: 35,
+      color: 'Aspen White',
+    },
+    {
+      id: 3,
+      name: 'Basic Charcoal Tee',
+      href: '#',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-03.jpg',
+      imageAlt: "Front of women's Basic Tee in Charcoal.",
+      price: 35,
+      color: 'Charcoal',
+    },
+    {
+      id: 4,
+      name: 'Artwork Tee',
+      href: '#',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-04.jpg',
+      imageAlt: "Front of women's Artwork Tee.",
+      price: 35,
+      color: 'Artwork',
+    }
+  ]
+
+  export default {
+    setup() {
+      return {
+        products
+      }
+    }
+  }
+</script>
